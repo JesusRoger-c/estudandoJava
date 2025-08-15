9a6ba948293b19540fc3ab47e440a0ede9c4bc84
Roger Gabriel <jesusrogercosta2004@gmail.com>
2025-08-14 21:21:39 -0300

    Usando alguns recursos de interações de repetições e condições para um processo seletivo

--- Desafio_ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
+++ Desafio_ProcessoSeletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,4 +1,5 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -6,19 +7,77 @@ public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("PROCESSO SELETIVO");
 		
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+		for(String candidato: candidatos) {
+			EntrarEmContato(candidato);
+			
+		}
+		
 
 	}
 	
+	//metodo entrar em contato
+	static void EntrarEmContato(String candidato) {
+		int tentativasRealizadas = 1;
+		boolean ContinuaTentando = true;
+		boolean Atendeu = false;
+		
+		do {
+			Atendeu = atender();
+			ContinuaTentando = !Atendeu;
+			if(ContinuaTentando) {
+				tentativasRealizadas++;
+			}else {
+				System.out.println("CONTATO REALIZADO COM SUCESSO!!!");
+			}
+			
+		}while(ContinuaTentando && tentativasRealizadas < 3); 
+		
+		if(Atendeu) {
+			System.out.println("CONSEGUIMOS CONTATO COM A(O) " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+		}else {
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas + " REALIZADAS");
+		}
+		
+	}
+	
+	//metodo auxiliar 
+	static boolean atender() {
+		return new Random().nextInt(3)==1;
+	}
+	
+	static void imprimirSelecionados() {
+		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+		System.out.println("Imprimindo a lista de selecionados, informando o indice do elemento.");
+		
+		for(int indice=0; indice < candidatos.length; indice++) {
+			System.out.println("O candidato de numero " + (indice+1) + " é o (a)" + candidatos[indice]);
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	//Selecionar colaborador
 	static void selecaoCandidatos() {
 		String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"}; 
 	    int candidatosSelecionados = 0;
 		int candidatoAtual = 0;
 		double salarioBase = 2000.0;
-		while(candidatosSelecionados < 5) {
+		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
 			String candidato = candidatos[candidatoAtual];
 			double salarioSugerido = valorPretendido(); 
 			
-			System.out.println("O candidato " + candidato + "Solicitou este valor de salario:" + salarioSugerido);
+			System.out.println("O candidato " + candidato + " Solicitou este valor de salario:" + salarioSugerido);
+			if(salarioBase >= salarioSugerido) {
+				System.out.println("O candidato: " + candidato + " foi selecionado para a vaga.");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
 		}
 	}
 	
