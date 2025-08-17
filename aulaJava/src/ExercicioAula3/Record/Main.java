2d4fd0a9cf860c81291daf9d1bd1731081cc649f
Roger Gabriel <jesusrogercosta2004@gmail.com>
2025-08-16 21:45:08 -0300

    Abordando o método Record e suas necessidades.

---
+++ aulaJava/src/ExercicioAula3/Record/Main.java
@@ -0,0 +1,12 @@
+package ExercicioAula3.Record;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var person = new PessoaRecord("João", 18);
+		System.out.println(person.idade());
+		System.out.println(person.name());
+
+	}
+
+}
---
+++ aulaJava/src/ExercicioAula3/Record/PessoaRecord.java
@@ -0,0 +1,9 @@
+package ExercicioAula3.Record;
+
+
+//O record serve para trabalhar com objetos imutáveis 
+public record PessoaRecord(String name, int idade) {
+	
+	
+
+}
